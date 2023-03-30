585b1664a4be3c4f4d334646cf19835b59cef572
olive <olionoleo@gmail.com>
2023-03-30 10:36:27 +0900

    Audio_playbackground_repeat_volume control #18

--- View/SceneView/SequenceView/SequenceThreeView.swift
+++ View/SceneView/SequenceView/SequenceThreeView.swift
@@ -6,9 +6,45 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct SequenceThreeView: View {
-    var body: some View {
-        Text("Scene Three View")
+        let audioPlayer = AVPlayer(url: Bundle.main.url(forResource: "sfx_sounds_interaction1", withExtension: "wav")!)
+        let volume: Float = 0.02
+        let playCount = 6
+        let timeGap: Double = 0.5
+    
+        @State private var currentPlayCount = 0
+    
+        var body: some View {
+            Image("BigCoffee_Back_1")
+                .padding()
+                .onAppear {
+                    // Start playing the audio when the view appears
+                    /*self.audioPlayer.play()
+                    self.audioPlayer.volume = self.volume*/
+                    let _ = Timer.scheduledTimer(withTimeInterval: self.timeGap + self.audioPlayer.currentItem!.asset.duration.seconds, repeats: true) { timer in
+                                       if self.currentPlayCount < self.playCount {
+                                           self.audioPlayer.seek(to: CMTime.zero)
+                                           self.audioPlayer.volume = self.volume
+                                           self.audioPlayer.play()
+                                           self.currentPlayCount += 1
+                                       } else {
+                                           timer.invalidate()
+                                       }
+                                   }
+                }
+                .onDisappear {
+                    // Stop playing the audio when the view disappears
+                    self.audioPlayer.pause()
+                }
+        }
+}
+
+
+// 따로 확인하기 위한 뷰입니다.
+struct SequenceThreeView_Previews: PreviewProvider{
+    static var previews: some View{
+        SequenceThreeView()
     }
 }
