3b90b70fceed4deda43cbe4e6a9f653bc7f995c5
보경 <97583162+bokoo14@users.noreply.github.com>
2023-03-31 00:26:52 +0900

    feat:  move sequence1 object

--- View/SceneView/SceneObjectView/JolJolView.swift
+++ View/SceneView/SceneObjectView/JolJolView.swift
@@ -8,7 +8,24 @@
 import SwiftUI
 
 struct JolJolView: View {
+    var start: (Float, Float) // 시작 위치
+    var end: (Float, Float) // 끝 위치
+    
+    @State var imageOffset: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //Image("Object_WaterPurifier")
+        
+        VStack {
+            Image("Object_WaterPurifier")
+                .position(x: CGFloat(start.0), y: CGFloat(start.1))
+                .edgesIgnoringSafeArea(.all)
+                .offset(x:  (imageOffset ? CGFloat(end.0) : 0), y: imageOffset ? CGFloat(end.1) : 0)
+                .onAppear{
+                    withAnimation(.linear(duration: 1.0)){
+                        self.imageOffset.toggle()
+                    }
+                }
+        }
     }
 }
